bba66ed78b8a118a83b096239f2b0f5d06f37bf5
ionchik <69267618+ionchik@users.noreply.github.com>
2024-11-19 10:16:43 +0300

    Update settings.py

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-eyutda6cg&0ljwv8xpi#d-+-rkxpu^ny$%@rz=_n8x!$!(rr9m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://ionchik-kalambetmission-b863.twc1.net"]
+ALLOWED_HOSTS = ["ionchik-kalambetmission-b863.twc1.net"]
 LOGIN_URL = '/users/login/'
 
 # Application definition
